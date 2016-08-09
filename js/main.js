f658c9b759bdfb787036d3a7d099f17633131be7
Cezary Prokopowicz <cezary.prokopowicz@pagepro.pl>
2016-08-09 18:15:17 +0200

    Moved prevent dafault

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 (function ($) {
     "use strict";
-	var $filters = $('.js-filters');
-	var $portfolioList = $('.js-portfolio-list');
-	var $item = $portfolioList.find('.item');
-	var filter;
-	
-	$filters.on('click', 'a', function(e) {
-		var $this = $(this);
-		filter = $this.data('filter');
-		e.preventDefault();
-		if($this.hasClass('active')) {
-			return;
-		}
-		$filters.find('a').removeClass('active');
-		$this.addClass('active');
-		if(filter === 'all') {
-			$item.show();
-			return;
-		}
-		$item.each(function () {
-			var $this = $(this);
-			$this.hide();
-			var tags = $this.find('figcaption').data('tags');
-			if (!((tags.indexOf(filter) > -1) === false)) {
-				$this.show();
-			}
-		});
-	});
+    var $filters = $('.js-filters');
+    var $portfolioList = $('.js-portfolio-list');
+    var $item = $portfolioList.find('.item');
+    var filter;
+    
+    $filters.on('click', 'a', function(e) {
+        var $this = $(this);
+        filter = $this.data('filter');
+
+        if($this.hasClass('active')) {
+            return;
+        }
+
+        $filters.find('a').removeClass('active');
+        $this.addClass('active');
+
+        if(filter === 'all') {
+            $item.show();
+            return;
+        }
+
+        $item.each(function () {
+            var $this = $(this);
+            $this.hide();
+            var tags = $this.find('figcaption').data('tags');
+            if (!((tags.indexOf(filter) > -1) === false)) {
+                $this.show();
+            }
+        });
+        e.preventDefault();
+    });
 }(jQuery));
